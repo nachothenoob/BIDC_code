7592d3c4a75fc351a51a08b1d708134fca13a538
nachothenoob <113204215+nachothenoob@users.noreply.github.com>
2023-05-22 12:20:06 -0400

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req,res){
+ res.writeHead(200,{ 'Content-Type': 'text/html' })
+ fs.readFile('index.html', function(error,data) {
+  if (error){
+   res.writeHead(404)
+   res.write('Error:File Not Found')
+  }
+ })
  res.write('Hello Node')
  res.end()
 })
